1b146b5071869d807d1be8271d1177b4293b3726
Jinhaker <32000520+Jinhaker@users.noreply.github.com>
2017-09-28 23:35:34 -0700

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,19 @@
+document.getElementById("open").addEventListener("click",function(){
+    document.getElementById("control").style.left = "10px"; 
+});
+
+document.getElementById("bg1").addEventListener("click", function(){
+     document.getElementById("backgroundImage").style.backgroundImage ="url(img/img1.jpg)";
+});
+
+document.getElementById("bg2").addEventListener("click", function(){
+     document.getElementById("backgroundImage").style.backgroundImage = "url(img/img2.jpg)";
+});
+
+document.getElementById("bg3").addEventListener("click", function(){
+     document.getElementById("backgroundImage").style.backgroundImage = "url(img/img3.jpg)";
+});
+
+document.getElementById("bg4").addEventListener("click", function(){
+     document.getElementById("backgroundImage").style.backgroundImage = "url(img/img4.jpg)";
+});
